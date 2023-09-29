dce5f96b9f6e0dcb8fd4c465147dcce20a711cd5
Deepakjhajhria <deepakjhajhria4657@gmail.com>
2023-09-29 21:46:18 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,7 @@ let body = document.querySelector("body");
 let span1 = document.querySelector(".span1")
 let span2 = document.querySelector(".span2")
 let span3 = document.querySelector(".span3")
-let home = document.querySelector(".home")
-let misson = document.querySelector(".misson")
-let training = document.querySelector(".training")
-let history = document.querySelector(".history")
-let forum = document.querySelector(".forum")
-let exhibition = document.querySelector(".exhibition")
+let navbtn = document.querySelector(".nav-btn")
 
 menu.addEventListener("click", function () {
     mobileView.classList.toggle("show");
@@ -18,42 +13,7 @@ menu.addEventListener("click", function () {
     span2.classList.toggle("span5");
     span3.classList.toggle("span6");
 })
-home.addEventListener("click", function () {
-    mobileView.classList.remove("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.remove("span4");
-    span2.classList.remove("span5");
-    span3.classList.remove("span6");
-})
-misson.addEventListener("click", function () {
-    mobileView.classList.remove("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.remove("span4");
-    span2.classList.remove("span5");
-    span3.classList.remove("span6");
-})
-training.addEventListener("click", function () {
-    mobileView.classList.remove("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.remove("span4");
-    span2.classList.remove("span5");
-    span3.classList.remove("span6");
-})
-history.addEventListener("click", function () {
-    mobileView.classList.remove("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.remove("span4");
-    span2.classList.remove("span5");
-    span3.classList.remove("span6");
-})
-forum.addEventListener("click", function () {
-    mobileView.classList.remove("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.remove("span4");
-    span2.classList.remove("span5");
-    span3.classList.remove("span6");
-})
-exhibition.addEventListener("click", function () {
+navbtn.addEventListener("click", function () {
     mobileView.classList.remove("show");
     body.classList.remove("overflow-hidden");
     span1.classList.remove("span4");

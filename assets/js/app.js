f779ad9b406df321fda2087d569f08dc1367d894
Deepakjhajhria <deepakjhajhria4657@gmail.com>
2023-09-30 10:32:55 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,22 +20,3 @@ navbtn.addEventListener("click", function () {
     span2.classList.remove("span5");
     span3.classList.remove("span6");
 })
-let mybutton = document.getElementById("btn-back-to-top");
-window.onscroll = function () {
-  scrollFunction();
-};
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-mybutton.addEventListener("click", backToTop);
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
